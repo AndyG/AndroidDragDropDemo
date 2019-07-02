dc8b9624142e073dc30de8d3c85c5c34f45e845b
AndyG <anderson.garron@gmail.com>
2019-07-02 15:21:34 -0700

    have a lot of drag and drop working

--- app/src/main/java/com/discord/androiddragdropdemo/linear/LinearActivity.kt
+++ app/src/main/java/com/discord/androiddragdropdemo/linear/LinearActivity.kt
@@ -4,23 +4,38 @@ import android.content.ClipData
 import android.content.ClipDescription
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.DragEvent
 import android.view.View
+import android.widget.LinearLayout
 import android.widget.TextView
-import androidx.core.content.ContextCompat
 import com.discord.androiddragdropdemo.R
+import com.discord.androiddragdropdemo.utils.dpToPx
+import kotlin.IllegalStateException
 
 class LinearActivity : AppCompatActivity() {
 
+    private lateinit var linearLayout: LinearLayout
     private lateinit var redCircle: TextView
     private lateinit var blueCircle: TextView
+    private lateinit var greenCircle: TextView
+    private lateinit var placeholderView: View
+
+    private var itemSize: Float = 0f
+    private var halfItemSize: Float = 0f
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_linear)
         bindViews()
+        linearLayout.removeView(placeholderView)
         configureDragAndDrop()
+
+        itemSize = dpToPx(128 + 16, resources)
+        halfItemSize = itemSize / 2
     }
+
+    private var draggedView: View? = null
 
     private fun configureDragAndDrop() {
         redCircle.setOnLongClickListener { view ->
@@ -28,48 +43,131 @@ class LinearActivity : AppCompatActivity() {
             val dragData = ClipData(redCircle.text, arrayOf(ClipDescription.MIMETYPE_TEXT_PLAIN), item)
             val shadow = View.DragShadowBuilder(view)
 
-            val isDragStarting = view.startDrag(dragData, shadow, null, 0)
-            view.setBackgroundResource(R.drawable.ring_black)
-            isDragStarting
+            view.visibility = View.GONE
+            val curIndex = linearLayout.indexOfChild(redCircle)
+            linearLayout.addView(placeholderView, curIndex)
+            draggedView = view
+            view.startDrag(dragData, shadow, null, 0)
+
+            true
         }
 
-        redCircle.setOnDragListener { view, event ->
-            when (event.action) {
-                DragEvent.ACTION_DRAG_ENDED -> {
-                    redCircle.setBackgroundResource(R.drawable.circle_red)
-                    redCircle.invalidate()
-                    true
+        blueCircle.setOnLongClickListener { view ->
+            val item = ClipData.Item(blueCircle.text)
+            val dragData = ClipData(blueCircle.text, arrayOf(ClipDescription.MIMETYPE_TEXT_PLAIN), item)
+            val shadow = View.DragShadowBuilder(view)
+
+            view.visibility = View.GONE
+            val curIndex = linearLayout.indexOfChild(blueCircle)
+            linearLayout.addView(placeholderView, curIndex)
+            draggedView = view
+            view.startDrag(dragData, shadow, null, 0)
+
+            true
+        }
+
+        greenCircle.setOnLongClickListener { view ->
+            val item = ClipData.Item(greenCircle.text)
+            val dragData = ClipData(greenCircle.text, arrayOf(ClipDescription.MIMETYPE_TEXT_PLAIN), item)
+            val shadow = View.DragShadowBuilder(view)
+
+            view.visibility = View.GONE
+            val curIndex = linearLayout.indexOfChild(greenCircle)
+            linearLayout.addView(placeholderView, curIndex)
+            draggedView = view
+            view.startDrag(dragData, shadow, null, 0)
+
+            true
+        }
+
+        linearLayout.setOnDragListener { v, event ->
+            if (event.action == DragEvent.ACTION_DRAG_ENDED) {
+                draggedView = null
+            } else if (event.action == DragEvent.ACTION_DRAG_LOCATION) {
+                val existingPlaceholderIndex = getPlaceholderIndex()
+                val draggedItemViewIndex = linearLayout.indexOfChild(draggedView)
+                val adjustedDraggedItemIndex = when {
+                    existingPlaceholderIndex != null && existingPlaceholderIndex < draggedItemViewIndex -> draggedItemViewIndex - 1
+                    else -> draggedItemViewIndex
                 }
-                else -> true
+
+                val y = event.y
+                val firstItemCenter = halfItemSize
+                val secondItemCenter = (itemSize) + halfItemSize
+                // this doesn't take into account placeholders.
+                val targetIndex = if (y < firstItemCenter) {
+                    0
+                } else if (y < secondItemCenter) {
+                    1
+                } else {
+                    2
+                }
+
+                val isDownwardMove = targetIndex > adjustedDraggedItemIndex
+
+                Log.v("findme", "linearlayout got DRAG_LOCATION event. y: ${event.y}. itemSize: $itemSize. computed index: $targetIndex")
+
+                val targetPlaceholderViewIndex = when {
+                    isDownwardMove -> targetIndex + 1 // the GONE view is affecting the computation, so add 1 to account for it.
+                    else -> targetIndex
+                }
+
+                // add the placeholder
+                if (existingPlaceholderIndex == null) {
+                    linearLayout.addView(placeholderView, targetPlaceholderViewIndex)
+                } else if (targetPlaceholderViewIndex != existingPlaceholderIndex){
+                    // need to move the placeholder.
+                    linearLayout.removeViewAt(existingPlaceholderIndex)
+                    linearLayout.addView(placeholderView, targetPlaceholderViewIndex)
+                }
+            } else if (event.action == DragEvent.ACTION_DROP) {
+                val placeholderIndex = getPlaceholderIndex() ?: throw IllegalStateException("drop with no placeholder")
+                val draggedItemIndex = linearLayout.indexOfChild(draggedView)
+
+                linearLayout.removeView(placeholderView)
+                linearLayout.removeView(draggedView)
+
+                val adjustedDropIndex = if (draggedItemIndex < placeholderIndex) placeholderIndex - 1 else placeholderIndex
+                linearLayout.addView(draggedView, adjustedDropIndex)
+
+                draggedView?.visibility = View.VISIBLE
             }
+            true
         }
-
-        blueCircle.setOnDragListener { view, event ->
-            when (val action = event.action) {
-                DragEvent.ACTION_DRAG_STARTED -> true
-                DragEvent.ACTION_DRAG_ENTERED -> {
-                    blueCircle.text = "!"
-                    true
-                }
-                DragEvent.ACTION_DRAG_EXITED -> {
-                    blueCircle.text = "2"
-                    true
-                }
-                DragEvent.ACTION_DROP -> {
-                    blueCircle.text = "3"
-                    true
-                }
-                else -> false
-            }
-        }
-    }
-
-    private fun handleDraggedOnto() {
-
     }
 
     private fun bindViews() {
+        linearLayout = findViewById(R.id.linear_layout)
         redCircle = findViewById(R.id.red_circle)
         blueCircle = findViewById(R.id.blue_circle)
+        greenCircle = findViewById(R.id.green_circle)
+        placeholderView = findViewById(R.id.placeholder_view)
+        placeholderView.tag = TAG_PLACEHOLDER
+    }
+
+    private fun findViewByClipData(data: ClipData): View {
+        val text = data.getItemAt(0).text
+        for (i in 0..linearLayout.childCount) {
+            val view = linearLayout.getChildAt(i)
+            if (view is TextView && view.text == text) {
+                return view
+            }
+        }
+
+        throw IllegalStateException("failed to find view with text: $text")
+    }
+
+    private fun getPlaceholderIndex(): Int? {
+        for (i in 0..linearLayout.childCount) {
+            if (linearLayout.getChildAt(i).tag == TAG_PLACEHOLDER) {
+                return i
+            }
+        }
+
+        return null
+    }
+
+    companion object {
+        private const val TAG_PLACEHOLDER = "placeholder"
     }
 }
--- app/src/main/java/com/discord/androiddragdropdemo/utils/DimensionUtils.kt
+++ app/src/main/java/com/discord/androiddragdropdemo/utils/DimensionUtils.kt
@@ -3,7 +3,7 @@ package com.discord.androiddragdropdemo.utils
 import android.content.res.Resources
 import android.util.TypedValue
 
-private fun dpToPx(dp: Int, resources: Resources): Float {
+fun dpToPx(dp: Int, resources: Resources): Float {
     return TypedValue.applyDimension(
         TypedValue.COMPLEX_UNIT_DIP,
         dp.toFloat(),
